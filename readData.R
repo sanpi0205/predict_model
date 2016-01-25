c84e8d6584afc8fd21416857302704db6383647f
sanpi0205 <sanpi0205@gmail.com>
2016-01-25 12:36:08 +0800

    读取数据

--- readData.R
+++ readData.R
@@ -2,7 +2,24 @@
 
 library('xlsx')
 library('dplyr')
+source('util.R')
 
 # 读取数据
-energy = read.xlsx2("")
+load(file = "final.RData")
+
+# 结果调整
+names(result)
+
+# 读取数据
+wb = loadWorkbook("../scenario.xlsx")
+ws = getSheets(wb)
+sheet = ws$scenario1
+
+# 读取数据
+total = read_two_columns(sheet, 46)
+coal = read_two_columns(sheet, 52)
+oil= c(7.5, 7.9)
+gas = c()
+primary_electricity = c()
+wind = c()
 
