a6df128930a94d4afa19f372b503f9cd2ea03b9a
sanpi0205 <sanpi0205@gmail.com>
2016-01-25 13:38:16 +0800

    修改总量

--- readData.R
+++ readData.R
@@ -1,25 +1,38 @@
 # 加载数据包
-
 library('xlsx')
 library('dplyr')
 source('util.R')
 
 # 读取数据
-load(file = "final.RData")
+load(file = "../final.RData")
 
 # 结果调整
 names(result)
+new_result = result
 
 # 读取数据
 wb = loadWorkbook("../scenario.xlsx")
 ws = getSheets(wb)
 sheet = ws$scenario1
 
-# 读取数据
+# 读取能源消费数据
 total = read_two_columns(sheet, 46)
+# 指数化数据
+total = exponent_interpolation(total, 6)
+predict_index=16:21
+# 修改总量
+new_result$energy_consumption$total[predict_index] = total
+
+
+
+
+total = approx(total, n =6)$y
+
 coal = read_two_columns(sheet, 52)
-oil= c(7.5, 7.9)
-gas = c()
-primary_electricity = c()
-wind = c()
+oil = read_two_columns(sheet, 53)
+gas = read_two_columns(sheet, 54)
+primary_electricity = read_two_columns(sheet, 55)
+
+
+
 
